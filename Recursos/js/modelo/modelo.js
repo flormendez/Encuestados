d025fd878d9e07deeeea4f191b48dc01d25e53e2
Florencia <florenciamendez93@gmail.com>
2018-12-17 10:44:15 -0300

    Guía 1. Falta corregir funcionalidad borrar pregunta

---
+++ Recursos/js/modelo/modelo.js
@@ -0,0 +1,51 @@
+/*
+ * Modelo
+ */
+var Modelo = function() {
+  this.preguntas = [];
+  this.ultimoId = 0;
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this)
+  this.preguntaBorrada = new Evento (this)
+  this.preguntaEditada = new Evento (this)
+
+  
+  
+};
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    if(this.preguntas.length > 0){
+      for(var i = 0; i < this.preguntas.length; i++){
+        if(this.ultimoId < this.preguntas[i].id){
+          this.ultimoId = this.preguntas[i].id
+        }
+      }
+    }
+    return this.ultimoId;
+  },
+
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(nombre, respuestas) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    this.preguntas.push(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+  },
+
+  borrarPregunta: function(id) {
+       var index = this.preguntas.indexOf(id);
+       this.preguntas.splice(index, 1);
+       this.guardar();
+       this.preguntaBorrada.notificar();
+     
+    
+  },
+
+  //se guardan las preguntas
+  guardar: function(){
+  },
+};
